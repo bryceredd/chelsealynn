0cdd8e75a4f8147c81bfe20749edea23f042d19d
Bryce <bryce@i.tv>
2013-07-27 13:38:11 -0600

    added ggg

---
+++ ggg.js
@@ -0,0 +1,14 @@
+// example ggg.js. Delete what you don't need
+module.exports = {
+
+  // services
+  start: "node_modules/coffee-script/bin/coffee app.coffee",
+
+  // install
+  install: "npm install",
+
+  // servers to deploy to
+  servers: {
+    prod: "root@192.241.216.160"
+  }
+}
