a520c94141f2403c87577e3da4a05851623cc949
Bryce Redd <bryce@i.tv>
2013-11-23 11:12:57 -0700

    Initial Commit

---
+++ ios/ChelseaLynnPortraits/ChelseaLynnPortraits/RFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RFAppDelegate.h
+//  ChelseaLynnPortraits
+//
+//  Created by Bryce Redd on 11/23/13.
+//  Copyright (c) 2013 Bryce Redd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
